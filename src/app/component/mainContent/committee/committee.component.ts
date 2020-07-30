f60e4bdda0779d8622ba3f4098d3b80777ed40b1
Sudarshan Gaikwad <sudarshan.gaikwad@siemens.com>
2020-07-31 01:10:08 +0530

    Admission notice aded

--- src/app/component/mainContent/committee/committee.component.ts
+++ src/app/component/mainContent/committee/committee.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { committeeList } from 'src/app/constant/commitee.constant';
 
-@Component({
+ @Component({
   selector: 'app-committee',
   templateUrl: './committee.component.html',
   styleUrls: ['./committee.component.css']
 })
 export class CommitteeComponent implements OnInit {
 
+  readonly committeeList = committeeList;
   constructor() { }
 
   ngOnInit() {
---
+++ src/app/constant/commitee.constant.ts
@@ -0,0 +1,12 @@
+
+export const committeeList = [
+    { displayText: 'Anti Discrimination Cell', filename: './assets/documentation/cell/AntiDiscriminationCell.jpg', link: '' },
+    { displayText: 'Placement and Career Guidance Cell', filename: './assets/documentation/cell/TnPG.jpg', link: '' },
+    { displayText: 'Gender Sensitization Cell', filename: './assets/documentation/cell/GenderSensitizationCell.jpg', link: '' },
+    { displayText: 'Anti Ragging Commitee', filename: './assets/documentation/cell/AntiRaggingCommitee.jpg', link: '' }
+];
+
+export const admissionList = [
+    { displayText: 'First Year Student List', filename: './assets/documentation/student/First Year Student List.PDF', link: '' },
+    { displayText: 'Direct Second Year Student List', filename: './assets/documentation/student/Direct Second Year Student List.PDF', link: '' }
+];
