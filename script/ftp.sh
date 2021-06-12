772f344cfb01481bad9edaf4002707b987177a3f
Sudarshan Gaikwad <sudarshan.gaikwad@siemens.com>
2021-06-12 22:26:46 +0530

    ncftp added

--- script/ftp.sh
+++ script/ftp.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 ls
-#HOST='copakluj.org'
-#USER='u421383828.sudarshan'
-#PASSWD='P@ssw0rd'
-
+HOST='copakluj.org'
+USER='u421383828.sudarshan'
+PASSWD='P@ssw0rd'
 ftp -in $HOST <<END_SCRIPT
 user $USER $PASSWD
-mput dist/*
+binary
+mput  dist/*.*
 quit
 END_SCRIPT
 
-exit 0
+#exit 0
