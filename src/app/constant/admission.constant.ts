7e2925ecee7e33540ca86726148df3504f7f4cf0
Sudarshan Gaikwad <sudarshan.gaikwad@siemens.com>
2022-01-19 18:23:39 +0530

    Admission Notification

--- src/app/constant/admission.constant.ts
+++ src/app/constant/admission.constant.ts
@@ -1,5 +1,4 @@
 export const admissionLink = [
-    {displayText: 'Admission Notification 2020-21 (First Year B. Pharmacy)', filename: '../assets/documentation/admission/20-21/FE2021.jpg', link: ''},
-    {displayText: 'Admission Notification 2020-21 (Direct Second Year B. Pharmacy)', filename: '../assets/documentation/admission/20-21/DSE2021.jpg', link: ''},
+    {displayText: 'Admission Notification and Schedule', filename: '../assets/documentation/admission/21-22/Admission_Notification_and_Schedule.pdf', link: ''},
      {displayText: 'Result : May 2019', filename: '../assets/documentation/imp/res2019.PDF', link: ''}, 
 ];
